47a14b66a437acb48c4ee7ebafd648e3c5045260
FishdStick <michaelsalvania22@gmail.com>
2024-07-13 17:08:14 +0800

    INIT COMMITN'T

---
+++ src/main/java/com/rss/resurv/ReSurvApplication.java
@@ -0,0 +1,13 @@
+package com.rss.resurv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReSurvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReSurvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rss/resurv/ReSurvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rss.resurv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReSurvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

afb4b8291a57bb543a947aec928dc38cd43fa624
FishdStick <102510656+FishdStick@users.noreply.github.com>
2024-07-18 03:32:48 +0800

    changed customer_id to be unique so that it can be used as a foreign key in the reservations table

--- src/main/java/com/rss/resurv/model/Reservation.java
+++ src/main/java/com/rss/resurv/model/Reservation.java
@@ -12,7 +12,7 @@ public class Reservation {
 
     // Customer foreign key (one customer, many reservations)
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    @JoinColumn(name = "customer_id", referencedColumnName = "customer_id")
     private Customer customer;
 
     // entity attributes

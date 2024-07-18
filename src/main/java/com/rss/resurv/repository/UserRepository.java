fa31f06ea98718620fb32b785e4d3969c2bbe6c4
FishdStick <102510656+FishdStick@users.noreply.github.com>
2024-07-18 14:18:18 +0800

    changed UUID to Long. changed the subclass ids to be unique and have appropriate names

--- src/main/java/com/rss/resurv/repository/UserRepository.java
+++ src/main/java/com/rss/resurv/repository/UserRepository.java
@@ -1,9 +1,9 @@
-package com.rss.resurv.repository;
-
-import com.rss.resurv.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
-}
+//package com.rss.resurv.repository;
+//
+//import com.rss.resurv.model.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.UUID;
+//
+//public interface UserRepository extends JpaRepository<User, UUID> {
+//}
